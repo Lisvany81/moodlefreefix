f2ba5f83588986547f10aeaf7b2798d4a96ec84e
Lisvany81 <103855052+Lisvany81@users.noreply.github.com>
2022-07-13 08:49:45 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.environ.get('5544114225:AAHq8-uN5zsY8R5zjgWcFGskxApOk5HNYPY','')
+TG_API_ID = os.environ.get('16833554',)
+TG_API_HASH = os.environ.get('426f0931a8e39657bbef48506a5e533b','')
+TG_ADMIN_USER = os.environ.get('Lisva81','')
+ROOT_DIR = 'root'
